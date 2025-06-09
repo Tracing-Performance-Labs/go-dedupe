595c31b819d5da09c792c102a216452d9f99aeaf
Alexander Goussas <goussasalexander@gmail.com>
2025-06-08 22:47:44 -0500

    test: use testcontainers to run redis
    Closes #1

--- redis_table_test.go
+++ redis_table_test.go
@@ -5,21 +5,25 @@ import (
 )
 
 func TestConformsToInterface(t *testing.T) {
-	var table table[string]
-	table = NewRedisTable()
-	_ = table
+	withRedis(t, func() {
+		var table table[string]
+		table = NewRedisTable()
+		_ = table
+	})
 }
 
 func TestStoringValueReturnsIt(t *testing.T) {
-	tbl := NewRedisTable()
+	withRedis(t, func() {
+		tbl := NewRedisTable()
 
-	for _, s := range testStrings {
-		tbl.Store(s, s)
-	}
-
-	for _, s := range testStrings {
-		if ss, err := tbl.Lookup(s); ss != s || err != nil {
-			t.Errorf("Expected %s, got %s with error %v", s, ss, err)
+		for _, s := range testStrings {
+			tbl.Store(s, s)
 		}
-	}
+
+		for _, s := range testStrings {
+			if ss, err := tbl.Lookup(s); ss != s || err != nil {
+				t.Errorf("Expected %s, got %s with error %v", s, ss, err)
+			}
+		}
+	})
 }
