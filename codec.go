5ac92e24b3d4beef004c3721b667ddabcc6c5b60
Alexander Goussas <goussasalexander@gmail.com>
2025-06-08 19:20:00 -0500

    refactor: move encoding/decoding logic into Codec
    This makes it so that the table implementation can be just a dictionary and easily replaced.

--- codec.go
+++ codec.go
@@ -1,21 +1,40 @@
 package dedupe
 
-type Codec[T Repr] struct {
-	t table[T]
+// A codec can be used to convert a string into a a more compact representation.
+type Codec struct {
+	t    table[string]
+	repr ObjectRepr[string]
 }
 
-func NewCodec[T Repr](t table[T]) *Codec[T] {
+func NewCodec(t table[string], repr ObjectRepr[string]) *Codec {
 	// TODO: Use options pattern to configure the codec.
-	return &Codec[T]{t: t}
+	return &Codec{
+		t:    t,
+		repr: repr,
+	}
 }
 
 // Encode the provided value by obtaining a compact representation for it.
-func (c *Codec[T]) Encode(s string) T {
-	return c.t.StoreAsRepr(s)
+func (c *Codec) Encode(s string) string {
+	val, err := c.t.Lookup(s)
+	if err != nil {
+		// 1. Compute representation.
+		repr := c.repr.GetRepr(s)
+
+		// 2. Store representation in the table.
+		c.t.Store(s, repr)
+
+		// 3. Store the reverse mapping.
+		c.t.Store(repr, s)
+
+		// 3. Set val.
+		val = repr
+	}
+	return val
 }
 
 // Retrieve a value from its compact representation.
-func (c *Codec[T]) Decode(o T) string {
-	// TODO
-	return ""
+func (c *Codec) Decode(repr string) (string, error) {
+	val, err := c.t.Lookup(repr)
+	return val, err
 }
--- table.go
+++ table.go
@@ -1,9 +1,8 @@
 package dedupe
 
 type table[T Repr] interface {
-	// This methods finds an appropriate representation for the string s and returns it.
-	StoreAsRepr(s string) T
-
-	// Recovers the original object from its representation.
-	RecoverFromRepr(repr T) (string, error)
+	// Store a value in the table.
+	Store(s string, repr T)
+	// Try to retrieve a value from the table.
+	Lookup(s string) (T, error)
 }
